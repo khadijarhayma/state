e259fb5008bdac652db8ccb588499de7e3e7854a
unknown <khadijarhayma@gmail.com>
2022-01-04 22:52:03 +0100

    commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState, useEffect } from "react";
+
+
+const App = ()=> {
+  const [show, setshow] = useState(false);
+
+  const handleClick = () => {
+    setshow(!show);
+  };
+  const [person, setperson] = useState({
+    fullName: "khadija",
+    bio: "bio",
+    imgSrc: "https://cdn.pixabay.com/photo/2015/03/14/00/00/landscape-672456_960_720.jpg",
+    profession: "web devoloper",
+  });
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handleClick}>show me</button>
+      {show ? (
+        <div>
+          <h1> Fullname: {person.fullName}</h1>
+          <h1>Bio:{person.bio}</h1>
+          <h1>Profession:{person.profession}</h1>
+          <img src={person.imgSrc} alt={person.fullName} />
+        </div>
+      ) : null}
+      <h1>The component has been rendered for {count} seconds</h1>
+     
     </div>
   );
 }
